334581d4307b36ddca50e493ab939b6bf700b2a6
Fatemeh Motamed <motamed.258@gmail.com>
2024-06-09 18:38:33 +0330

    Adding login example to 5.py

--- Term1/5.py
+++ Term1/5.py
@@ -6,3 +6,18 @@ elif age <= 0:
 else:
     year = 18 - age
     print(f"شما {year} سال بعد می‌توانید اقدام به دریافت گواهینامه کنید.")
+
+
+email = "admin@poulstar.org"
+passs = "1234"
+enterEmail = input("Please enter your email: ")
+if "@gmail.com" in enterEmail:
+    print("Invalid Email!")
+elif email == enterEmail:
+    enterPass = input("Please enter your password: ")
+    if passs == enterPass:
+        print("Login!")
+    else:
+        print("Invalid Password!")
+else:
+    print("You need to signup first!")
